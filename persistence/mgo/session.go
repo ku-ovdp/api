b7a3a8d5fd5fc89949803adb7ecf527d40e9f9e8
Travis Cline <travis.cline@gmail.com>
2013-05-10 18:25:35 -0500

    Add mgo backend

---
+++ persistence/mgo/session.go
@@ -0,0 +1,71 @@
+package mgo
+
+import (
+	"fmt"
+	. "github.com/ku-ovdp/api/entities"
+	. "github.com/ku-ovdp/api/repository"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
+)
+
+func (m *mgoBackend) NewSessionRepository(repositories RepositoryGroup) SessionRepository {
+	if m.db == nil {
+		panic("Not initialized! Missing call to Init()?")
+	}
+	sr := &sessionRepository{
+		m,
+		m.db.C("sessions"),
+		repositories["projects"].(ProjectRepository),
+	}
+
+	return sr
+}
+
+type sessionRepository struct {
+	backend  *mgoBackend
+	c        *mgo.Collection
+	projects ProjectRepository
+}
+
+func (sr *sessionRepository) Get(id int) (Session, error) {
+	result := Session{}
+	q := sr.c.Find(bson.M{"id": id})
+	err := q.One(&result)
+	return result, err
+}
+
+func (sr *sessionRepository) Put(session Session) (Session, error) {
+	if session.Id == 0 {
+		session.Id = sr.backend.nextId("session")
+	}
+	_, err := sr.c.Upsert(bson.M{"id": session.Id}, session)
+	if err != nil {
+		return Session{}, err
+	}
+	return sr.Get(session.Id)
+}
+
+func (sr *sessionRepository) Remove(id int) error {
+	return fmt.Errorf("Not implemented")
+}
+
+func (sr *sessionRepository) Scan(projectId int, from, to int) ([]Session, error) {
+	results := []Session{}
+
+	if _, err := sr.projects.Get(projectId); err != nil {
+		if err == NotFound {
+			err = fmt.Errorf("Project with id %d not found.", projectId)
+		}
+		return results, err
+	}
+
+	query := bson.M{"id": bson.M{"$gte": from}, "projectid": projectId}
+	if to > 0 {
+		query["id"].(bson.M)["$lte"] = to
+	}
+
+	q := sr.c.Find(query)
+	err := q.All(&results)
+
+	return results, err
+}
