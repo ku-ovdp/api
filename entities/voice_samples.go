9cf6c198d8a988609492d356e3f8e00460af10e1
Travis Cline <travis.cline@gmail.com>
2013-05-11 00:18:56 -0500

    Enhance VoiceSample model

--- entities/voice_samples.go
+++ entities/voice_samples.go
@@ -5,10 +5,12 @@ import (
 )
 
 type VoiceSample struct {
+	Id        int
 	SessionId int
 	Created   time.Time
 	Length    time.Duration
 	MimeType  string
+	Size      int
 	Bitrate   int
 	AudioURL  string `json:"-"`
 }
